2da26dd98968257e05e94e58f972c61272a22f2f
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2016-09-25 23:49:37 +0300

    algorithm divide on forward and backward pass

---
+++ solve.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.5
+
+import sys
+import numpy 
+from gauss import gauss
+
+print("Input file is", sys.argv[1])
+in_file = open(sys.argv[1], 'r')
+N = int(in_file.readline())
+print("N =", N)
+eps = float(in_file.readline())
+print("eps =", eps)
+
+b = numpy.empty(N, numpy.float64)
+for i in range(0, N):
+    b[i] = numpy.float64(in_file.readline())
+print("b =\n", b)
+
+A = numpy.empty((N, N) , numpy.float64)
+for i in range(0, N):
+    for j in range(0, N):
+        A[i][j] = numpy.float64(in_file.readline())
+print("A =\n", A)
+
+gauss(A, b, N, eps)
+
+print("b =\n", b)   
+print("A =\n", A)
+
+print("Output file is", sys.argv[2])
+out_file = open(sys.argv[2], 'w')
+for i in range(0, N):
+    out_file.write(str(b[i]) + "\n")
