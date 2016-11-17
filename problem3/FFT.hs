e0208c824051028c2f65d45bb9e368cc6d1b3d87
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2016-11-17 23:38:18 +0300

    arrow

--- problem3/FFT.hs
+++ problem3/FFT.hs
@@ -1,14 +1,11 @@
 module FFT where
 
 import Data.Complex
+import Data.List (partition)
+import Control.Arrow
 
 phaseRot :: RealFloat a => Int -> Complex a -> Int -> Complex a
 phaseRot nSmpls x m = x * cis(-2 * pi * fromIntegral(m) / fromIntegral(nSmpls))
-
-splitByParity :: [a] -> ([a], [a])
-splitByParity [] = ([], [])
-splitByParity [x] = ([x], [])
-splitByParity (x:y:zs) = (x:xt, y:yt) where (xt, yt) = splitByParity zs  
 
 fft :: RealFloat a => [Complex a] -> [Complex a]
 fft [] = []
@@ -18,4 +15,4 @@ fft zs = (zipWith (+) es rotOs) ++ (zipWith (-) es rotOs)
     where es = fft evens
           rotOs = zipWith (phaseRot (length zs)) os [0..]
           os = fft odds
-          (evens, odds) = splitByParity zs
+          (evens, odds) = ((map fst) *** (map fst)) (partition (\(x,y) -> even y) (zip zs [0..])) 
